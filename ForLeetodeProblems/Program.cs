da4f3fd40380b4c114dc5dade1333a5034e9cfd4
Ulug'bek <user@JUMABOYEV-UL>
2025-10-29 09:26:12 +0500

    ADD NEW SOLUTION

--- ForLeetodeProblems/Program.cs
+++ ForLeetodeProblems/Program.cs
@@ -203,6 +203,17 @@ namespace ForLeetodeProblems
     }
     public class Solution 
     {
+        public int MinBitFlips(int start, int goal)
+        {
+            int count = 0;
+            int x = start ^ goal;
+            while (x > 0)
+            {
+                count += (x & 1);
+                x <<= 1;
+            }
+            return count;
+        }
         public int SmallestNumber(int n)
         {
             if (n == 1) return 1;
