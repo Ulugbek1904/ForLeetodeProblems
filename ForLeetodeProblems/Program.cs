ba53295f5d7972dc9f4b431cda6f51933746ab4f
Ulugbek <julugbek023@gmail.com>
2025-12-23 23:52:48 +0500

    add

--- ForLeetodeProblems/Program.cs
+++ ForLeetodeProblems/Program.cs
@@ -74,6 +74,35 @@ namespace ForLeetodeProblems
     }
     public class Solution
     {
+        public int ClimbStairs(int n)
+        {
+            if (n == 1 || n == 2)
+                return n;
+            int f = 1;
+            int s = 2;
+            int t = 0;
+            for(int i = 3; i <= n; i++)
+            {
+                t = f + s;
+                f = s;
+                s = f;
+            }
+
+            return t;
+        }
+
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            HashSet<int> set = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i > k)
+                    set.Remove(nums[i - k - 1]);
+                if (!set.Add(nums[i]))
+                    return true;
+            }
+            return false;
+        }
         public int MinDeletionSize(string[] strs)
         {
             int n = strs[0].Length;
